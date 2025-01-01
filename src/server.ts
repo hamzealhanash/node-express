ab11976debe22647014a62818f7388782587cdd0
hamzealhanash <hamzaalhanash2005@gmail.com>
2025-01-01 21:59:10 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,32 @@
-import fs = require('fs')
 import 'dotenv/config'
+import fs = require('fs')
 import http = require('http')
 import https = require('https')
 import express = require('express')
-import {Application} from 'express'
-import bodyParser = require('body-parser')
 import useRouter = require('./routes/auth')
+import bodyParser = require('body-parser')
+import {Application} from 'express'
 
-const privateKey = fs.readFileSync('sslcert/server.key', 'utf8')
-const certificate = fs.readFileSync('sslcert/server.crt', 'utf8')
+const privateKey = fs.readFileSync(__dirname + '/../keys/selfsigned.key', 'utf8')
+const certificate = fs.readFileSync(__dirname + '/../keys/selfsigned.crt', 'utf8')
 const credentials = {key: privateKey, cert: certificate}
 const app = express()
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials, app)
-const PORT = process.env.PORT || 3000
+const HTTP_PORT = process.env.HTTP_PORT || 2000
+const HTTPS_PORT = process.env.HTTPS_PORT || 2443
+
 app.use(bodyParser.urlencoded({limit: "10mb", extended: false}))
 app.use(express.json())
 
-
 app.use('/api/auth', useRouter as Application)
 app.get("/", (_req: any, res: any) => {
-    res.send('Hello, Express!')
+    res.send("test")
 })
 
-httpServer.listen(PORT, () => {
-    console.log(`HTTP server running on PORT: http://localhost:${PORT}`)
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP server running on PORT: http://localhost:${HTTP_PORT}`)
 })
-httpsServer.listen(PORT, () => {
-    console.log(`HTTPS server running on PORT: https://localhost:${PORT}`)
+httpsServer.listen(HTTPS_PORT, () => {
+    console.log(`HTTPS server running on PORT: https://localhost:${HTTPS_PORT}`)
 })
